cf02e7a5bbe31a090d64486b824287c625ff31fd
Esinulo Glory Adaeze <daeze7@gmail.com>
2022-12-08 15:52:38 -0800

    Task Hello put

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints a given phrase
+ * return: 0 if edited properly , non-zero if not
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0)
+}
