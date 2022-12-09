f4b75f402bf8e1309c3c4299f769b19ebaec22ae
Esinulo Glory Adaeze <daeze7@gmail.com>
2022-12-08 19:58:31 -0800

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main- enter the phrase
+ * Description: 'the programs description'
+ * Return: zero if exited, otherwise non-zero
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
