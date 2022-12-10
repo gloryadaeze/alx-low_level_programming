d7f8e936d19f5d67b4235a3f46a0f8c9b1b84fc0
Esinulo Glory Adaeze <daeze7@gmail.com>
2022-12-09 18:54:06 -0800

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ * description: describe program
+ * Return: Alway zero
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+
+		putchar(n);
+
+	if (n != 57)
+	{
+		putchar(',');
+		putchar(' ');
+	}
+
+	putchar("\n");
+	return (0);
+}
