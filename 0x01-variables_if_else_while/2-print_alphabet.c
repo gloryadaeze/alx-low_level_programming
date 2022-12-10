f43df54ccdf3d714e57a60ad79f64d7626999d61
Esinulo Glory Adaeze <daeze7@gmail.com>
2022-12-09 17:32:20 -0800

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Description: 'the programs description'
+ * Return: Always zero
+ */
+int main(void)
+{
+	char lc;
+
+	for (lc = 'a'; lc <= 'z'; lc++)
+	{
+		putchar(lc);
+	}
+	putchar('\n');
+	return (0);
+}
