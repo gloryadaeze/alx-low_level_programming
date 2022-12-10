0ec0218f9bb267ecf4b5b84b2fb70b1d0650e3d1
Esinulo Glory Adaeze <daeze7@gmail.com>
2022-12-09 21:50:20 -0800

    Task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,19 +8,18 @@
 
 int main(void)
 {
-	int n;
-
-	for (n = 0; n <= 9; n++)
-	{
-	putchar(n + '0');
-
-	if (n < 9)
-	{
-	putchar(',');
-	putchar(' ');
-	}
-	}
-	putchar('\n');
+		int n;
+		for (n = 0; n <= 9; n++)
+		{
+		putchar(n + '0');
+		
+		if (n < 9)
+		{
+		putchar(',');
+		putchar(' ');
+		}
+		}
+		putchar('\n');
 	
 	return (0);
 }
