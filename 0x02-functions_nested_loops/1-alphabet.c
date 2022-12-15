8c58294694a6f5fae965d44af8eefb6403b572d9
Esinulo Glory Adaeze <daeze7@gmail.com>
2022-12-14 22:46:41 -0800

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print abc
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void);
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	-putchar('\n');
+}
