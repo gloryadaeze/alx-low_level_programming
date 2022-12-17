0158f39b453191035d6d2c60780bfd467a6edde8
Esinulo Glory Adaeze <daeze7@gmail.com>
2022-12-16 20:03:49 -0800

    the task

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_most_numbers - checker
+ * Return: always 0
+ */
+
+void print_most_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 10; 1++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j >= 10)
+				_putchar('1');
+			_putchar (j % 10 + "0");
+		}
+		_putchar('\n');
+	}
+}
