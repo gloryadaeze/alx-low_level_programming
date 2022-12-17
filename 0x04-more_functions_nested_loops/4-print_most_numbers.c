588406a90e1a07d88071c43dfb883967ae6ee123
Esinulo Glory Adaeze <daeze7@gmail.com>
2022-12-16 21:10:02 -0800

    task 6

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -11,9 +11,7 @@ void print_most_numbers(void)
 	for (i = 48; i < 58; i++)
 	{
 		if (i != 50 && i != 52)
-		{
 			_putchar(i);
-		}
-		_putchar('\n');
 	}
+	_putchar('\n');
 }
---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_line - print
+ * @n: num
+ * Return: 0
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar(95);
+	}
+	_putchar('\n');
+}
