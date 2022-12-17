baef28b59d274ba5467fd33e0de4064c73d15ac1
Esinulo Glory Adaeze <daeze7@gmail.com>
2022-12-16 19:44:21 -0800

    task 3

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * mul - checker
+ * @c: char to check
+ * Return: 0 or 1
+ */
+
+int mul(int a, int b)
+{
+
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - print
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+
+}
