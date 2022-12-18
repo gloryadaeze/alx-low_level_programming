5a4df738e00d63bd3d7ca03cd194076c901e2a9f
Esinulo Glory Adaeze <daeze7@gmail.com>
2022-12-17 20:34:52 -0800

    task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_diagonal - checker
+ * @n: number
+ * Return: no return
+ */
+
+void print_diagonal(int n);
+{
+	int i, j;
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+		{
+			_putchar(' ');
+		}
+		_putchar(92);
+		if (i < (n - 1))
+		{
+			_putchar('\n');
+		}
+	}
+}
