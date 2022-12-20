a6ceeba3838a59c409c51e444e0a60c65b890f23
Esinulo Glory Adaeze <daeze7@gmail.com>
2022-12-20 07:40:03 -0800

    task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_to_98 - check
+ * @n: integer
+ * Return: 0 or 1
+ */
+
+void print_to_98(int n)
+{
+	while (n < 98)
+	{
+		printf("%i, ", n);
+		n++;
+	}
+	while (n > 98)
+	{
+		printf("%i, ", n);
+		n--;
+	}
+	printf("98");
+	_putchar('\n');
+}
